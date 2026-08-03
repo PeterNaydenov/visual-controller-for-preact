d5681d3fc00229cf4c1da4324043a2b7d504464a
Peter Naydenov <peter.naydenov@gmail.com>
2026-08-03 07:14:03 +0300

    Types update.- v.2.0.0

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,76 +1,44 @@
-export type VisualControllerOptions = {
-    /**
-     * - Object with dependencies that should be available for all components
-     */
-    dependencies?: any;
-};
-export type UpdateMethods = {
-    /**
-     * : string] - Any method registered via setupUpdates
-     */
-    methodName?: Function;
-};
-export type PreactComponentProps = {
-    /**
-     * - Dependencies provided during VisualController initialization
-     */
-    dependencies: any;
-    /**
-     * - Data passed as second argument to publish
-     */
-    data: any;
-    /**
-     * - Function to register external update methods
-     */
-    setupUpdates: Function;
-};
-export type VisualControllerAPI = {
-    /**
-     * - Publish a Preact app
-     */
-    publish: Function;
-    /**
-     * - Destroy a Preact app
-     */
-    destroy: Function;
-    /**
-     * - Get app update methods
-     */
-    getApp: Function;
-    /**
-     * - Check if app exists
-     */
-    has: Function;
+export type SetCallback = (markers: {
+    start: Text;
+    end: Text;
+}) => string | void;
+export type SetupUpdates = Object;
+export type VisualControllerInstance = {
+    set: SetCallback & ((fn: SetCallback, ...args: any[]) => void);
+    publish: (alias: string, component: any, data?: object, extraParams?: object) => Promise<SetupUpdates | false>;
+    destroy: (target?: string | string[]) => boolean | number;
+    has: (alias: string) => boolean;
+    getApp: (alias: string) => SetupUpdates | false;
+    isEmpty: (alias: string) => boolean | undefined;
+    list: () => string[];
+    reset: () => void;
 };
 /**
- * Configuration options for VisualController
- * @typedef {Object} VisualControllerOptions
- * @property {Object} [dependencies] - Object with dependencies that should be available for all components
+ *  Callback that places dim markers into the DOM.
+ *  @callback SetCallback
+ *  @param {{ start: Text, end: Text }} markers
+ *  @returns {string | void}
  */
 /**
- * Methods exposed for external component control
- * @typedef {Object} UpdateMethods
- * @property {Function} [methodName: string] - Any method registered via setupUpdates
+ *  Object passed to `setupUpdates` from inside a published component.
+ *  @typedef {Object} SetupUpdates
  */
 /**
- * Props passed to Preact components
- * @typedef {Object} PreactComponentProps
- * @property {Object} dependencies - Dependencies provided during VisualController initialization
- * @property {Object} data - Data passed as second argument to publish
- * @property {Function} setupUpdates - Function to register external update methods
+ *  Controller instance returned by `VisualController`.
+ *  @typedef {Object} VisualControllerInstance
+ *  @property {SetCallback & ((fn: SetCallback, ...args: any[]) => void)} set
+ *  @property {(alias: string, component: any, data?: object, extraParams?: object) => Promise<SetupUpdates | false>} publish
+ *  @property {(target?: string | string[]) => boolean | number} destroy
+ *  @property {(alias: string) => boolean} has
+ *  @property {(alias: string) => SetupUpdates | false} getApp
+ *  @property {(alias: string) => boolean | undefined} isEmpty
+ *  @property {() => string[]} list
+ *  @property {() => void} reset
  */
 /**
- * VisualController return object
- * @typedef {Object} VisualControllerAPI
- * @property {Function} publish - Publish a Preact app
- * @property {Function} destroy - Destroy a Preact app
- * @property {Function} getApp - Get app update methods
- * @property {Function} has - Check if app exists
+ *  Visual Controller for Preact
+ *  @param {Object} [dependencies={}]
+ *  @returns {VisualControllerInstance}
  */
-/**
- * Visual Controller for Preact
- * @param {Object} [dependencies={}] - Dependencies that should be available for all components
- * @returns {VisualControllerAPI} - Object with methods: publish, destroy, getApp, has
- */
-declare function VisualController(dependencies?: any): VisualControllerAPI;
+declare function VisualController(dependencies?: any): VisualControllerInstance;
 export default VisualController;
